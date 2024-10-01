c00dec84a59bb7e27050b770b6c5263517ca36cd
Mario Ledesma <mledesma313@gmail.com>
2024-09-30 21:05:39 -0400

    color picker workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
 // Write your Color component here
-
+const Color = ({ color, setSelectedColor, selectedColor }) => {
+  // console.log(`is ${color} the selcted color?`, color === selectedColor)
+  const isSelected = color === selectedColor ? "selected": ""
+  // console.log(isSelected)
+  return <div className={`${color} ${isSelected}`} onClick={() => setSelectedColor(color)}></div>;
+};
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="yellow" selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
+        <Color color="green" selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
+        <Color color="black" selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
+      </div>
     </div>
   );
 };
